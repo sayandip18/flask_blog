4cc14e95f0976d54765e83ab63c371c0dbd7ebe2
Sayandip Halder <sayandiph4@gmail.com>
2021-10-19 02:15:24 +0530

    forms

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
-
+from flask.templating import render_template_string
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '70ad1cc4f66ff8fab0c7e40b0715aaac'
 
 posts = [
     {
@@ -23,6 +26,16 @@ def hello_world():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
