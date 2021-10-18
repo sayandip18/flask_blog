0350fa60dcf2cb5d7a4c35ec97314803d15c11d9
Sayandip Halder <sayandiph4@gmail.com>
2021-10-19 01:54:32 +0530

    create basic html, add bootstrap, css

--- flaskblog.py
+++ flaskblog.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author': 'Sayandip Halder',
+        'title': 'First post',
+        'content': 'lorem ipsum'
+    },
+    {
+        'author': 'Potai Halder',
+        'title': 'Second post',
+        'content': 'lorem ipsum'
+    }
+]
+
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+if __name__ == "__main__":
+    app.run(debug=True)
