0ae0792cb84ceb9b8d3585929b788e05be5777ad
Sayandip Halder <sayandiph4@gmail.com>
2021-10-19 21:49:20 +0530

    login and registration features added

--- flaskblog.py
+++ flaskblog.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
 from flask.templating import render_template_string
 from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
@@ -20,21 +20,29 @@ posts = [
 
 @app.route("/")
 @app.route("/home")
-def hello_world():
+def home():
     return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
 
-@app.route("/register")
+@app.route("/register", methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'You have been logged in!', 'success')
+        return redirect(url_for('home'))
+    else:
+        flash('Login unsuccessful. Please check email or password :(', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 if __name__ == "__main__":
